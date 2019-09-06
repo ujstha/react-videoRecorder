85e04597770031cabefbd6db8e51b73d27aa4f61
ujstha <ujstha27@gmail.com>
2019-09-06 23:30:34 +0300

    build

---
+++ build/precache-manifest.786bcb9091a77cb21f76f99800f7def8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b420ffab91f4b4c172daeb80412cc480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9c5f57a83fbbd6ba7e",
+    "url": "/static/css/2.14686bf5.chunk.css"
+  },
+  {
+    "revision": "7624423097f1f7f6facd",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3a9c5f57a83fbbd6ba7e",
+    "url": "/static/js/2.96a05162.chunk.js"
+  },
+  {
+    "revision": "7624423097f1f7f6facd",
+    "url": "/static/js/main.f55a22ac.chunk.js"
+  },
+  {
+    "revision": "e5775aeb37cc4ff2db18",
+    "url": "/static/js/runtime~main.efe75c15.js"
+  }
+]);
